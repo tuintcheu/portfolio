868ff557ea7f2e980a572a5248504b6d6aace505
Tuintcheu pharel <128803370+tuintcheu@users.noreply.github.com>
2025-09-30 16:53:08 +0100

    Update rasengan.config.js

--- rasengan.config.js
+++ rasengan.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "rasengan";
 export default defineConfig({
   server: {
     host: true, // ← autorise les connexions externes
-    allowedHosts: ["portfolio-87is.onrender.com"], // ← ton domaine Render
+    allowedHosts: 'all', // ← ton domaine Render
   },
 });
